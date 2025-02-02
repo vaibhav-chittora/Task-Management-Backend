7ed2e087c466be1591e11fa9f64d6312962344a3
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-02-02 23:10:15 +0530

    added user Repository

---
+++ src/repositories/user.js
@@ -0,0 +1,63 @@
+import User from "../schema/user.js";
+
+export const createUser = async (data) => {
+  try {
+    const user = await User.create(data);
+    return user;
+  } catch (error) {
+    console.log("Error in createUser Repository", error);
+    throw error;
+  }
+};
+
+export const getUserByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email });
+    return user;
+  } catch (error) {
+    console.log("Error in getUserByEmail Repository", error);
+    throw error;
+  }
+};
+
+export const getUserByUsername = async (username) => {
+  try {
+    const user = await User.findOne({ username });
+    return user;
+  } catch (error) {
+    console.log("Error in getUserByUsername Repository", error);
+    throw error;
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    console.log("Error in getAllUsers Repository", error);
+    throw error;
+  }
+};
+
+export const updateUserById = async (id, updateObject) => {
+  try {
+    const response = await User.findByIdAndUpdate(id, updateObject, {
+      new: true,
+    });
+    return response;
+  } catch (error) {
+    console.log("Error in updateUser Repository", updateUserById);
+    throw error;
+  }
+};
+
+export const deleteUserById = async (id) => {
+  try {
+    const response = await User.findByIdAndDelete(id);
+    return response;
+  } catch (error) {
+    console.log("Error in deleteUser Repository", error);
+    throw error;
+  }
+};
