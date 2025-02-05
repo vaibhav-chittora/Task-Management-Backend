f5d1f35fc6b59d17243df1b101abe267f0293f9a
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-02-05 22:52:51 +0530

    added CORS

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import { PORT } from "./config/serverConfig.js";
 import connectDB from "./config/dbConfig.js";
 import apiRouter from "./routes/apiRouter.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //api routes here
 app.use("/api", apiRouter);
